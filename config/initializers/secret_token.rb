fb1489721d61bec262d6e3a7bee85adefe0add17
Javier Benito García-Mochales <javibgm@hotmail.com>
2013-10-23 16:56:11 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MixApp::Application.config.secret_key_base = '0e2bec129395e2bb7e963a5610299a61461a5a91f3de8677eee2837e67bfe9c74b12bdb08f875e31d897e78a2704cb365b96dcbe97382fac1f5275954ea547a7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MixApp::Application.config.session_store :cookie_store, key: '_mix_app_session'
